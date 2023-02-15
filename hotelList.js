0688fc4e1dd7a282a7c6101bb27825cb98fa6673
yuki-o1o5 <jkksst1712@gmail.com>
2023-02-15 15:35:54 -0800

    create hotel list chat bot

---
+++ hotelList.js
@@ -0,0 +1,37 @@
+var Amadeus = require("amadeus");
+const readline = require("readline");
+
+var amadeus = new Amadeus({
+  clientId: "HRRgFa8o9rFWCOEgKAHYVoXZ8TfIGGpa",
+  clientSecret: "Q6tA45nGUlAvyzSv",
+});
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question(
+  "Please enter the city code. We can search the hotel list for you! : ",
+  (answer) => {
+    let modifyAnswer = answer.toUpperCase();
+
+    amadeus.referenceData.locations.hotels.byCity
+      .get({
+        cityCode: modifyAnswer,
+      })
+      .then(function (response) {
+        response.data.forEach((element) => {
+          console.log("===================================================");
+          console.log(element.name);
+          console.log(element.geoCode);
+        });
+        let numberOfHotel = response.data;
+        console.log("===================================================");
+        console.log(`Total number of hotels is : ${numberOfHotel.length}`);
+      })
+      .catch(function (response) {
+        console.error(response);
+      });
+  }
+);
