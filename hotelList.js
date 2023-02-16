d9591d61efa688b8d95a5916e14e64771cc97754
yuki-o1o5 <jkksst1712@gmail.com>
2023-02-15 16:01:52 -0800

    changed data

--- hotelList.js
+++ hotelList.js
@@ -15,19 +15,20 @@ rl.question(
   "Please enter the city code. We can search the hotel list for you! : ",
   (answer) => {
     let modifyAnswer = answer.toUpperCase();
-
     amadeus.referenceData.locations.hotels.byCity
       .get({
         cityCode: modifyAnswer,
       })
       .then(function (response) {
         let numberOfHotel = response.data;
-        console.log("===================================================");
+        console.log("=====================================================");
         console.log(`Total number of hotels is : ${numberOfHotel.length}`);
         response.data.forEach((element) => {
-          console.log("===================================================");
-          console.log(element.name);
-          console.log(element.geoCode);
+          console.log("=====================================================");
+          console.log(`Hotel name: ${element.name}`);
+          console.log(
+            `Geo code  : latitude: ${element.geoCode.latitude}, longitude: ${element.geoCode.longitude}`
+          );
         });
       })
       .catch(function (response) {
